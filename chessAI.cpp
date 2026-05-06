182a8e27a89f35119e6ec887cfd1990c882beb04
mahaupt <marcel@ehaupt.de>
2026-05-06 15:07:33 +0200

    Add parallel move calculations

--- chessAI.cpp
+++ chessAI.cpp
@@ -13,6 +13,7 @@
 #include <algorithm>
 #include <chrono>
 #include <cstdlib>
+#include <future>
 #include <stdexcept>
 
 
@@ -27,9 +28,17 @@ class SearchTimeout : public std::runtime_error {
 public:
     SearchTimeout(): std::runtime_error("search timeout") {}
 };
+
+struct RootSearchResult {
+    Move move;
+    int value = -99999999;
+    unsigned long long nodesEvaluated = 0;
+    bool completed = false;
+    int moveIndex = 0;
+};
 }
 
-ChessAI::ChessAI():startEbene(1), completedDepth(0), nodesEvaluated(0), timeBudgetSeconds(2.0)  {};
+ChessAI::ChessAI():startEbene(1), completedDepth(0), nodesEvaluated(0), timeBudgetSeconds(3.0)  {};
 
 Move ChessAI::getNextMove(CBoard & board, int color) {
     completedDepth = 0;
@@ -37,18 +46,95 @@ Move ChessAI::getNextMove(CBoard & board, int color) {
     searchDeadline = std::chrono::steady_clock::now() + std::chrono::duration_cast<std::chrono::steady_clock::duration>(std::chrono::duration<double>(timeBudgetSeconds));
     
     Move bestMove = Move();
+    std::vector<Move> rootMoves = std::vector<Move>();
+    board.getLegalMoves(color, rootMoves);
+    if (rootMoves.size() <= 0) {
+        return bestMove;
+    }
+    sortMoves(board, rootMoves);
     
     for (int depth = 1; ; depth++) {
-        Move depthMove = Move();
-        startEbene = depth;
-        
-        try {
-            doAllMoves(board, color, startEbene, depthMove);
-            bestMove = depthMove;
-            completedDepth = depth;
-        } catch (const SearchTimeout&) {
+        if (std::chrono::steady_clock::now() >= searchDeadline) {
             break;
         }
+        startEbene = depth;
+        nodesEvaluated++;
+        
+        Move depthBestMove = rootMoves[0];
+        int depthBestValue = -99999999;
+        int depthBestMoveIndex = 0;
+        
+        Move firstMove = rootMoves[0];
+        try {
+            firstMove.doMove(board);
+            Move ignoredMove = Move();
+            depthBestValue = -doAllMoves(board, color == 0 ? 1 : 0, depth - 1, ignoredMove);
+            firstMove.reverseMove(board);
+        } catch (const SearchTimeout&) {
+            firstMove.reverseMove(board);
+            break;
+        }
+        
+        std::vector<std::future<RootSearchResult>> futures = std::vector<std::future<RootSearchResult>>();
+        futures.reserve(rootMoves.size() - 1);
+        
+        for (int i = 1; i < rootMoves.size(); i++) {
+            Move rootMove = rootMoves[i];
+            auto deadline = searchDeadline;
+            int rootAlpha = depthBestValue;
+            
+            futures.push_back(std::async(std::launch::async, [this, &board, color, depth, rootMove, deadline, rootAlpha, i]() mutable {
+                RootSearchResult result;
+                result.move = rootMove;
+                result.moveIndex = i;
+                
+                ChessAI worker;
+                worker.searchDeadline = deadline;
+                worker.startEbene = depth - 1;
+                worker.nodesEvaluated = 0;
+                
+                CBoard boardCopy = board;
+                Move moveOnCopy = rootMove;
+                moveOnCopy.doMove(boardCopy);
+                
+                try {
+                    Move ignoredMove = Move();
+                    result.value = -worker.doAllMoves(boardCopy, color == 0 ? 1 : 0, depth - 1, ignoredMove, -99999999, -rootAlpha);
+                    result.completed = true;
+                } catch (const SearchTimeout&) {
+                    result.completed = false;
+                }
+                
+                result.nodesEvaluated = worker.nodesEvaluated;
+                return result;
+            }));
+        }
+        
+        bool depthCompleted = true;
+        
+        for (int i = 0; i < futures.size(); i++) {
+            RootSearchResult result = futures[i].get();
+            nodesEvaluated += result.nodesEvaluated;
+            
+            if (!result.completed) {
+                depthCompleted = false;
+                continue;
+            }
+            
+            if (result.value > depthBestValue
+                || (result.value == depthBestValue && result.moveIndex < depthBestMoveIndex)) {
+                depthBestValue = result.value;
+                depthBestMove = result.move;
+                depthBestMoveIndex = result.moveIndex;
+            }
+        }
+        
+        if (!depthCompleted) {
+            break;
+        }
+        
+        bestMove = depthBestMove;
+        completedDepth = depth;
         
         if (std::chrono::steady_clock::now() >= searchDeadline) {
             break;
